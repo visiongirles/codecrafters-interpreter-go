8ca5d6bc8edcb1c194274d759ded2e4875dc2217
Kate Sychenko <busy.sychenko@gmail.com>
2024-07-11 18:20:21 +0400

    codecrafters submit [skip ci]

--- cmd/myinterpreter/main.go
+++ cmd/myinterpreter/main.go
@@ -5,6 +5,19 @@ import (
 	"os"
 )
 
+// type OperatorType int
+
+// const (
+// 	StringType OperatorType = iota
+// 	RuneType
+// )
+
+// type OperatorValue struct {
+// 	Type        OperatorType
+// 	StringValue string
+// 	RuneValue   rune
+// }
+
 func main() {
 
 	hasError := false
@@ -21,6 +34,17 @@ func main() {
 		';': "SEMICOLON",
 		'*': "STAR",
 		'/': "SLASH",
+	}
+
+	operators := map[rune]string{
+		'=': "EQUAL",
+		// "==": "EQUAL_EQUAL",
+		'!': "BANG",
+		// "!=": "BANG_EQUAL",
+		'<': "LESS",
+		// "<=": "LESS_EQUAL",
+		'>': "GREATER",
+		// ">=": "GREATER_EQUAL",
 	}
 
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -48,12 +72,64 @@ func main() {
 	if len(fileContents) > 0 {
 		content := string(fileContents)
 		count := 1
-		for _, token := range content {
+		for index := 0; index < len(content); index++ {
+			token := rune(content[index])
 			if token == '\n' {
 				count++
 			}
 			if value, ok := singleTokens[token]; ok {
 				fmt.Printf("%s %c null\n", value, token)
+			} else if _, ok := operators[token]; ok {
+				var tokenType string
+				var lexeme string
+				switch token {
+				case '=':
+					{
+						tokenType = "EQUAL"
+						lexeme = string(token)
+						if index+1 < len(content) && content[index+1] == '=' {
+							// fmt.Printf("index+1: %d content[index+1]: %s ", index+1, string(content[index+1]))
+							tokenType = "EQUAL_EQUAL"
+							lexeme = string(token) + string(content[index+1])
+							index++
+						}
+					}
+
+				case '!':
+					{
+						tokenType = "BANG"
+						lexeme = string(token)
+						if index+1 < len(content) && content[index+1] == '=' {
+							// fmt.Printf("index+1: %d content[index+1]: %s ", index+1, string(content[index+1]))
+							tokenType = "BANG_EQUAL"
+							lexeme = string(token) + string(content[index+1])
+							index++
+						}
+					}
+				case '<':
+					{
+						tokenType = "LESS"
+						lexeme = string(token)
+						if index+1 < len(content) && content[index+1] == '=' {
+							// fmt.Printf("index+1: %d content[index+1]: %s ", index+1, string(content[index+1]))
+							tokenType = "LESS_EQUAL"
+							lexeme = string(token) + string(content[index+1])
+							index++
+						}
+					}
+				case '>':
+					{
+						tokenType = "GREATER"
+						lexeme = string(token)
+						if index+1 < len(content) && content[index+1] == '=' {
+							// fmt.Printf("index+1: %d content[index+1]: %s ", index+1, string(content[index+1]))
+							tokenType = "GREATER_EQUAL"
+							lexeme = string(token) + string(content[index+1])
+							index++
+						}
+					}
+				}
+				fmt.Printf("%s %s null\n", tokenType, lexeme)
 			} else {
 				fmt.Fprintf(os.Stderr, "[line %d] Error: Unexpected character: %c\n", count, token)
 				hasError = true
